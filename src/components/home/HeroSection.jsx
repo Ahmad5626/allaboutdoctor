6b2e7e59a4467cfcf476cc9dbdde7ea51fac482d
Ahmad raza <119778503+Ahmad5626@users.noreply.github.com>
2025-11-29 01:02:25 +0530

    popup new set

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -153,7 +153,7 @@ export default function HeroSection() {
         <div className="fixed inset-0 bg-black/60 backdrop-blur-sm flex items-center justify-center z-[9999] p-4">
 
           {/* POPUP BOX */}
-          <div className="bg-white rounded-2xl max-w-6xl w-full shadow-2xl overflow-hidden relative animate-fadeIn">
+          <div className="bg-white rounded-2xl max-w-4xl w-full shadow-2xl overflow-hidden relative animate-fadeIn">
             <button
               onClick={() => setShow(false)}
               className="absolute top-3 right-3 bg-black/70 text-white w-8 h-8 rounded-full flex items-center justify-center text-xl z-50"
@@ -167,18 +167,18 @@ export default function HeroSection() {
               <div className="p-8 flex flex-col justify-between">
                 
                 {/* Bullet Points */}
-                <div className="space-y-4 text-[#003b99] font-semibold text-lg">
+                <div className="space-y-4 text-[#003b99] font-semibold text-sm">
                   <p className="flex items-start gap-2">
-                    <span className="text-[#0057ff] text-2xl">✔</span> Expert Guidance for MBBS Admissions
+                    <span className="text-[#0057ff] text-sm">✔</span> Expert Guidance for MBBS Admissions
                   </p>
                   <p className="flex items-start gap-2 ml-6">
-                    <span className="text-[#0057ff] text-2xl">✔</span> University Selection Based on Budget & NEET Score
+                    <span className="text-[#0057ff] text-sm">✔</span> University Selection Based on Budget & NEET Score
                   </p>
                   <p className="flex items-start gap-2 ml-10">
-                    <span className="text-[#0057ff] text-2xl">✔</span> Complete Support: Documentation, Visa & Hostel
+                    <span className="text-[#0057ff] text-sm">✔</span> Complete Support: Documentation, Visa & Hostel
                   </p>
                   <p className="flex items-start gap-2 ml-16">
-                    <span className="text-[#0057ff] text-2xl">✔</span> Personal Counselling & Student Assistance
+                    <span className="text-[#0057ff] text-sm">✔</span> Personal Counselling & Student Assistance
                   </p>
                 </div>
 
@@ -186,31 +186,31 @@ export default function HeroSection() {
                 <div className=" mt-8 flex  justify-center">
                   <img
                     src="/assets/img/man-1.png"
-                    className="w-56 md:w-64  z-10"
+                    className="w-56 md:w-54  z-10"
                   />
                   <img
                     src="/assets/img/girl-1.png"
-                    className="w-44 md:w-54 object-cover  bottom-0"
+                    className="w-44 md:w-44 object-cover  bottom-0"
                   />
                 </div>
               </div>
 
               {/* RIGHT SIDE EXACT AS YOUR DESIGN */}
               <div className="p-8">
-                <h2 className="text-[34px] md:text-[34px] font-bold text-[#f68b1e] leading-tight text-center">
+                <h2 className="text-[20px] md:text-[26px] font-bold text-[#f68b1e] leading-tight text-center">
                   START YOUR MBBS <br /> JOURNEY WITH CONFIDENCE
                 </h2>
 
-                <p className="mt-3 text-[#003b99] font-semibold text-sm border-b pb-2">
+                <p className="mt-3 text-[#003b99] font-semibold text-[12px] border-b pb-2 text-center">
                   SECURE ADMISSION IN TOP MEDICAL UNIVERSITIES IN INDIA & ABROAD
                 </p>
 
                 {/* Icons Row */}
-                <p className="text-center mt-4 text-[14px] font-semibold text-[#003b99]">
+                <p className="text-center mt-4 text-[12px] font-semibold text-[#003b99]">
                   With All About Doctor Global Medical Admission Program
                 </p>
 
-                <div className="mt-3 grid grid-cols-4 gap-3 text-center text-[10px] font-semibold text-[#003b99]">
+                <div className="mt-3 grid grid-cols-4 gap-3 text-center text-[9px] font-semibold text-[#003b99]">
                   <div>
                     <img src="/assets/img/popup-icon-1.png" className="w-8 mx-auto" />
                     15,000+ <br /> STUDENTS GUIDED
@@ -234,12 +234,12 @@ export default function HeroSection() {
                   <input
                     type="text"
                     placeholder="Name"
-                    className="w-full border border-[#f68b1e] p-3 rounded-xl text-[#003b99] font-semibold"
+                    className="w-full border border-primary py-2 px-3 rounded-md text-[#003b99] "
                   />
                   <input
                     type="text"
                     placeholder="Mobile"
-                    className="w-full border border-[#f68b1e] p-3 rounded-xl text-[#003b99] font-semibold"
+                    className="w-full border border-primary py-2 px-3 rounded-md text-[#003b99] "
                   />
                 </div>
 
